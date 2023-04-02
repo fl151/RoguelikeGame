bb84dc0de2daf356526edee5e9b8a04016f64bfb
fl151 <118074384+fl151@users.noreply.github.com>
2023-04-02 22:11:27 +0700

    Delete Usings

--- RoguelikeProject/Assets/Scripts/PlayerMover.cs
+++ RoguelikeProject/Assets/Scripts/PlayerMover.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Tilemaps;
+
 
 public class PlayerMover : MonoBehaviour
 {
