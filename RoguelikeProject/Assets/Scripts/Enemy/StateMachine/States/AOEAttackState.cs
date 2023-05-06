9d15618c83e2bb45b0688b5ee25ddf7732b781d3
fl151 <118074384+fl151@users.noreply.github.com>
2023-05-06 17:03:48 +0700

    Start create aoe attack state

---
+++ RoguelikeProject/Assets/Scripts/Enemy/StateMachine/States/AOEAttackState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy), typeof(BulletPool))]
+public class AOEAttackState : State
+{
+    [SerializeField] private Bullet _template;
+    [SerializeField] private int _countBulletsInPool;
+
+    private BulletPool _pool;
+
+    private void Start()
+    {
+        InitPool();
+    }
+
+    private void InitPool()
+    {
+        _pool = GetComponent<BulletPool>();
+        GetComponent<Enemy>().Dead += DestroyPool;
+        _pool.Init(_template, _countBulletsInPool);
+    }
+
+    private void DestroyPool()
+    {
+        _pool.DestroyPool();
+    }
+}
